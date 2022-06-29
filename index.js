d71043d183583e27efe7f06306f248eb983a1e6c
Joanna Reggie Pasa <joannareggie@gmail.com>
2022-06-29 18:06:27 +0800

    export sayHello function

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const sayHello = (name)=>{
     console.log('***************');
-    console.log('Hello!', name);
+    console.log('Hello!!', name);
     console.log('***************');
 }
 
-sayHello('Joanna');
+module.exports = { sayHello };
